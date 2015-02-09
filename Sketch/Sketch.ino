63f40a8c30e06660034f16d636357d5e9c9d90a0
Alexei Matrosov <alexei.matrosov@hiqo-solutions.com>
2015-02-09 16:01:11 +0300

    Minor rename

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -5,9 +5,9 @@
 #include "UdpController.h"
 
 const int LOOP_DELAY = 100;
-int TOGGLE_PINS[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-const int TOGGLE_PINS_COUNT = 14;
-SwitchBus _switchBus(TOGGLE_PINS, TOGGLE_PINS_COUNT);
+int PINS[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+const int PINS_COUNT = 14;
+SwitchBus _switchBus(PINS, PINS_COUNT);
 UdpController _controller(&_switchBus);
 
 void setup()
