4a8bb022594675cbfea2e941498e5fefd98d06b7
Alexei Matrosov <matrosov.alexei@gmail.com>
2015-02-10 23:38:40 +0300

    Changed Udp protocol for switching pins

--- Sketch/UdpController.cpp
+++ Sketch/UdpController.cpp
@@ -27,16 +27,16 @@ void UdpController::Loop()
 {
     int packetSize = Udp.parsePacket();
 
-    if (packetSize && packetSize >= 4)
+    if (packetSize && packetSize >= 4 && packetSize % 2 == 0)
     {
         Udp.read(Buffer, UDP_TX_PACKET_MAX_SIZE);
 
         if (Buffer[0] == 0x28 && Buffer[1] == 0x07)
         {
-            int state = (int)Buffer[2];
-            for (int i = 3; i < packetSize; ++i)
+            for (int i = 3; i < packetSize; i+=2)
             {
                 int pinIndex = (int)Buffer[i];
+                int state = (int)Buffer[i+1];
                 _switchBus->Set(pinIndex, state);
             }
         }
